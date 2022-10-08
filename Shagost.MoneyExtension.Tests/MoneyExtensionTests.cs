ade1f0eac012c0ae7aa192471183a65397676c5b
Silvio Agostinho <shagost@hotmail.com>
2022-10-08 19:57:14 -0300

    Versao inicial

---
+++ Shagost.MoneyExtension.Tests/MoneyExtensionTests.cs
@@ -0,0 +1,14 @@
+namespace Shagost.MoneyExtension.Tests;
+
+[TestClass]
+public class MoneyExtensionTests
+{
+    [TestMethod]
+    public void ShouldConvertDecimalToInt()
+    {
+        decimal valor = 279.98M;
+        var cents = valor.ToCents();
+
+        Assert.AreEqual(27998, cents);
+    }
+}
